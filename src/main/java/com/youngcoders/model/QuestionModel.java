eabee2ac694a9c9211e4fc267570f8d8bbb90a16
dnovakovic15 <dnovakovic21@yahoo.com>
2018-03-01 11:49:14 -0600

    New Jar File

--- src/main/java/com/youngcoders/model/QuestionModel.java
+++ src/main/java/com/youngcoders/model/QuestionModel.java
@@ -1,24 +1,48 @@
 package com.youngcoders.model;
 
+import javax.persistence.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public class AnswersSerialized {
+@Entity
+@Table(name = "questions")
+public class QuestionModel {
 
-    public static byte[] toStream(ArrayList stu) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+
+    String objectDB;
+
+    public QuestionModel(){};
+
+    public QuestionModel(int id, String objectDB) {
+        this.id = id;
+        this.objectDB = objectDB;
+    }
+
+    public static byte[] toStream(Question obj) {
         // Reference for stream of bytes
         byte[] stream = null;
         // ObjectOutputStream is used to convert a Java object into OutputStream
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
              ObjectOutputStream oos = new ObjectOutputStream(baos);) {
-            oos.writeObject(stu);
+            oos.writeObject(obj);
             stream = baos.toByteArray();
         } catch (IOException e) {
             // Error in serialization
             e.printStackTrace();
         }
         return stream;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStreamDB() {
+        return objectDB;
     }
 }
